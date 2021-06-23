dfd16173405f50157df168c136f2bc60e45f2d2a
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-06-23 22:21:23 +0900

    Create baekjoon_1076.py

---
+++ baekjoon_1076.py
@@ -0,0 +1,7 @@
+
+
+rst = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey','white']
+one= rst.index(input()) 
+two = rst.index(input())
+three = rst.index(input())
+print(f'{(one*10+two)*(10**three)}')
