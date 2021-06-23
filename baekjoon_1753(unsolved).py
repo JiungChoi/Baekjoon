e8b3d0414341611495b6e4623bf1266ed4dd50d7
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-06-24 07:16:20 +0900

    Create baekjoon_1753(unsolved).py

---
+++ baekjoon_1753(unsolved).py
@@ -0,0 +1,16 @@
+import sys
+
+V, E = map(int,sys.stdin.readline().split())
+K = int(input())
+
+dist =['INF' for _ in range(V)]
+dist[K-1] = 0
+adj = [['INF' for _ in range(V)] for _ in range(V)]
+
+# E개의 경로와 가중치
+for i in range(E):
+    u, v, w = map(int,sys.stdin.readline().split())
+    adj[u-1][v-1] = str(w)
+
+for i in range(V):
+    dist[i] = min(dist[i],dist[K-1] + adj[K-1][i])
