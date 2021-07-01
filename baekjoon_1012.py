8de26c0e7f71cba3cbabc2f1039ade6db697ef2f
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-07-01 17:03:47 +0900

    Create baekjoon_1012.py

---
+++ baekjoon_1012.py
@@ -0,0 +1,9 @@
+import sys
+T = int(input())
+for _ in range(T):
+    M, N, K = map(int, sys.stdin.readline().split())
+    arry = [[[] for _ in range(M)]for _ in range(N)]
+    for _ in range(K):
+        arry_x, arry_y = map(int, sys.stdin.readline().split())
+        arry[arry_y][arry_x] = 1
+    print(arry)
