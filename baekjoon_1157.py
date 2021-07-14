0ecb4ee9b397be0666db9a90cafcbfcfb98517df
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-07-14 20:36:41 +0900

    Update baekjoon_1157.py

--- baekjoon_1157.py
+++ baekjoon_1157.py
@@ -1,15 +1,32 @@
 from sys import stdin
 
 dic = {}
-
 ary1 = list(stdin.readline())
 
 for i in ary1:
-    if  (122 >= ord(i) and (ord(i) >= 97):
-        if f'{chr(ord(ary1[i])-32)}' in dic.keys:
-            dic[f'{chr(ord(ary1[i])-32)}'] += 1
-        else: dic[f'{chr(ord(ary1[i])-32)}'] = 1
-    else:
-        if ary1[i] in dic.keys:
-            dic[ary1[i]] += 1
-        else: dic[ary1[i]] = 1
+    if  (122 >= ord(i)) and (ord(i) >= 97):
+        if chr(ord(i)-32) in dic:
+            dic[chr(ord(i)-32)] += 1
+        else: dic[chr(ord(i)-32)] = 1
+    elif i != '\n':
+        if i in dic:
+            dic[i] += 1
+        else: dic[i] = 1
+    else: pass
+
+cnt = 0
+for v in dic.values():
+    if cnt < v:
+        cnt = v
+
+n_cnt = 0
+for k,v in dic.items():
+    if v == cnt:
+        n_cnt += 1
+        if n_cnt > 1:
+            print('?')
+            break
+else: 
+    for k,v in dic.items():
+        if v == cnt:
+            print(k)
